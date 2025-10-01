31992a95399d8b5808e6036ba91f155eb14c6600
madaapril <mohammada.aprilianto@gmail.com>
2025-10-01 10:37:41 +0700

    comment recombee

--- src/app/api/sync/route.ts
+++ src/app/api/sync/route.ts
@@ -1,22 +1,22 @@
-import { client, rqs } from "@/lib/recombee";
+// import { client, rqs } from "@/lib/recombee";
 
-export async function GET(req) {
-  // contoh add item (funpaper)
-  //   await client.send(new rqs.AddItem("funpaper-123"));
+// export async function GET(req) {
+//   // contoh add item (funpaper)
+//   //   await client.send(new rqs.AddItem("funpaper-123"));
 
-  // set properties (judul, kategori, dll)
-  //   await client.send(new rqs.SetItemValues("funpaper-123", {
-  //     title: "Funpaper Matematika Dasar",
-  //     category: "Matematika",
-  //     difficulty: "Beginner"
-  //   }, { cascadeCreate: true }));
+//   // set properties (judul, kategori, dll)
+//   //   await client.send(new rqs.SetItemValues("funpaper-123", {
+//   //     title: "Funpaper Matematika Dasar",
+//   //     category: "Matematika",
+//   //     difficulty: "Beginner"
+//   //   }, { cascadeCreate: true }));
 
-  // contoh add user
-  try {
-    await client.send(new rqs.AddUser("user-456"));
-    return Response.json({ ok: true });
-  } catch (error) {
-    console.error(error);
-    return Response.json({ ok: false, error: error.message }, { status: 500 });
-  }
-}
+//   // contoh add user
+//   try {
+//     await client.send(new rqs.AddUser("user-456"));
+//     return Response.json({ ok: true });
+//   } catch (error) {
+//     console.error(error);
+//     return Response.json({ ok: false, error: error.message }, { status: 500 });
+//   }
+// }
--- src/lib/recombee.js
+++ src/lib/recombee.js
@@ -1,12 +1,12 @@
-import recombee from 'recombee-js-api-client';
-import { AddUser } from 'recombee.requests';
+// import recombee from 'recombee-js-api-client';
+// import { AddUser } from 'recombee.requests';
 
-// console.log(Object.keys(recombee))
+// // console.log(Object.keys(recombee))
 
-export const rqs = { AddUser };
-// export const rqs = recombee.requests;
-export const client = new recombee.ApiClient(
-  process.env.RECOMBEE_DB_ID,      // dari dashboard
-  process.env.RECOMBEE_PRIVATE_TOKEN, // pakai private key
-  { region: 'ap-se' } // sesuaikan region db kamu
-);
+// export const rqs = { AddUser };
+// // export const rqs = recombee.requests;
+// export const client = new recombee.ApiClient(
+//   process.env.RECOMBEE_DB_ID,      // dari dashboard
+//   process.env.RECOMBEE_PRIVATE_TOKEN, // pakai private key
+//   { region: 'ap-se' } // sesuaikan region db kamu
+// );
