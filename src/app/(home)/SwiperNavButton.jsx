bf28bb9f1e8604f22d930762d8d26c639098bcac
madaapril <mohammada.aprilianto@gmail.com>
2025-10-04 13:19:30 +0700

    slider home

--- src/app/(home)/SwiperNavButton.jsx
+++ src/app/(home)/SwiperNavButton.jsx
@@ -4,7 +4,7 @@ export const SwiperNavButton = () => {
     const swiper = useSwiper();
 
   return (
-    <div className="swiper-nav-btns absolute bottom-10 z-100 left-1/2 lg:left-0 -translate-x-1/2 lg:-translate-x-0">
+    <div className="swiper-nav-btns lg:w-full absolute bottom-10 z-100 left-1/2 lg:left-0 -translate-x-1/2 lg:-translate-x-0">
       <div className="container flex flex-row gap-5">
         <button
           className="rounded-full bg-white/80 hover:bg-white/70 p-1 w-8 h-8 text-[#DEAE53] font-medium shadow-xl"
