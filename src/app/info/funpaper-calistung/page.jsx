785dc45e7e8ee316a485e4315345f5a707a7acee
madaapril <mohammada.aprilianto@gmail.com>
2025-10-07 15:14:19 +0700

    landing page calistung

--- src/app/info/funpaper-calistung/page.jsx
+++ src/app/info/funpaper-calistung/page.jsx
@@ -19,62 +19,61 @@ export default function page() {
     <main className="text-gray-800 -mt-[68px]">
       <style jsx>
         {`
-          .text-biru {
-            color: #0055a0;
+          .text-hijau {
+            color: #0085a0;
           }
         `}
       </style>
 
       {/* Hero */}
-      <section className="py-20 px-8 text-center bg-[url(/images/bg/bg-harian-1.avif)] bg-cover bg-no-repeat h-[1800px] sm:h-[1500px] lg:h-[1300px] overflow-hidden">
+      <section className="py-20 px-8 text-center bg-[url(/images/bg/bg-calistung.jpg)] bg-cover bg-top bg-no-repeat h-[1900px] sm:h-[1700px] lg:h-[1500px] overflow-hidden">
         <div className="container px-4 relative">
           <div
             className="mt-24 transition-transform duration-75 space-y-60 sm:space-y-80"
             style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
           >
             <div className="space-y-10">
-              <h1 className="text-3xl md:text-5xl text-biru font-bold leading-tight mb-6">
-                Jadi Lebih Kreatif<br />Dengan Funpaper!
+              <h1 className="text-3xl md:text-5xl text-hijau font-bold leading-tight mb-6">
+                Dapatkan Gratis
+                <br />
+                Funpaper Calistung
               </h1>
               <p className="text-lg md:text-xl font-medium">
-                Kumpulan lembar kerja dengan aktivitas yang fokus pada keterampilan dasar anak usia dini seperti pengenalan angka dan huruf, kemampuan motorik halus (menggunting, menebalkan garis, menulis), kreativitas seni (mewarnai, menggambar), serta aktivitas kognitif sederhana seperti maze dan soal campuran.
+                Rahasia ananda lancar calistung tanpa kendala
               </p>
-              <Link href="/funpaper-calistung" className="tombol-biru-tua">
+              <Link
+                href="/funpaper-calistung"
+                className="tombol-hijau bg-[#0085A0]!"
+              >
                 Download Sekarang
               </Link>
             </div>
 
             <div className="">
               <div className="text-center mb-8">
-                <h2 className="text-2xl sm:text-4xl font-bold leading-tight mb-4 text-biru">
-                  Dengan Funpaper Harian Anak Bisa
+                <h2 className="text-2xl sm:text-3xl font-bold leading-tight mb-4 text-hijau">
+                  Sebagai orang tua pasti ingin memberikan yang terbaik untuk
+                  anaknya, termasuk dalam mendampingi anak belajar membaca,
+                  menulis dan berhitung. Tapi sering terkendala karena
                 </h2>
               </div>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 text-md">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-md">
                 {[
                   {
-                    img: "/images/assets/penggaris.png",
-                    text: "Mengenal konsep angka, huruf, bentuk geometri, pola, & konsep dasar matematika",
+                    img: "/images/assets/koin.png",
+                    text: "Biaya les privat mahal",
                   },
                   {
-                    img: "/images/assets/huruf.png",
-                    text: "Latihan pengenalan huruf dan kosa kata sederhana",
+                    img: "/images/assets/komputer-silang.png",
+                    text: "Bimbel online kurang efektif",
                   },
                   {
-                    img: "/images/assets/lampu-2.png",
-                    text: "Meningkatkan kemampuan logika",
+                    img: "/images/assets/otak-petir.png",
+                    text: "Kesulitan buat anak mau belajar",
                   },
                   {
-                    img: "/images/assets/bola.png",
-                    text: "Melatih kemampuan mengenali pola visual dan perbedaan kecil antar objek",
-                  },
-                  {
-                    img: "/images/assets/menulis.png",
-                    text: "Memperkuat kontrol tangan dan koordinasi mata-tangan",
-                  },
-                  {
-                    img: "/images/assets/paint-3.avif",
-                    text: "Mengembangkan imajinasi dan kreativitas",
+                    img: "/images/assets/tanda-tanya.png",
+                    text: "Bingung memilih metode yang tepat",
                   },
                 ].map((item, idx) => (
                   <div
@@ -93,6 +92,31 @@ export default function page() {
                 ))}
               </div>
             </div>
+
+            <div className="flex flex-col lg:flex-row gap-4">
+              <div className="w-auto lg:w-200">
+                <Image
+                  src="/images/funpaper/calistung/funpaper-calistung-1.png"
+                  width={300}
+                  height={200}
+                  alt="calistung"
+                  className="mx-auto"
+                />
+              </div>
+              <div className="text-center lg:text-left lg:px-18">
+                <h3 className="text-xl sm:text-2xl font-bold leading-tight mb-1 text-hijau">
+                  Sekarang ada solusinya!
+                </h3>
+                <h2 className="text-2xl sm:text-3xl font-bold leading-tight mb-4 text-hijau">
+                  Funpaper Calistung dari Terus Mengajar
+                </h2>
+                <p className="font-medium">
+                  Funpaper Calistung hadir untuk memberikan teknik yang mudah
+                  dipraktikkan anak usia dini hingga awal SD agar menguasai
+                  keterampilan dasar membaca, menulis, dan berhitung.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -101,154 +125,44 @@ export default function page() {
         <div className="container">
           <div className="text-center">
             <h2 className="text-2xl md:text-4xl font-bold leading8 md:leading-12 mb-8">
-              Kenapa Harus Punya Funpaper Ini?
+              Dengan Funpaper Calistung anak bisa:
             </h2>
           </div>
-
-          <div className="mb-18 grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="space-y-10 mb-8">
             {[
               {
-                img: "/images/assets/free-2.png",
-                heading: "Gratis dan praktis",
-                text: "Bisa diunduh kapan saja, tinggal print, langsung siap dipakai",
+                img: "/images/funpaper/calistung/funpaper-calistung-2.png",
+                title: "Membaca tanpa mengeja",
+                text: "Menggunakan metode suku kata yang terbukti lebih mudah dipahami anak",
               },
               {
-                img: "/images/assets/check.png",
-                heading: "Variasi aktivitas setiap hari",
-                text: "Dengan banyak tema dan variasi aktivitas, pembelajaran jadi tidak monoton, anak tidak cepat bosan karena terus mendapat tantangan baru tiap hari.",
+                img: "/images/funpaper/calistung/funpaper-calistung-3.png",
+                title: "Menulis bertahap",
+                text: "Dimulai dari latihan pola garis, huruf, suku kata dan kata sederhana agar anak tidak merasa terbebani dan lebih percaya diri",
               },
               {
-                img: "/images/assets/palu.png",
-                heading: "Melatih banyak kemampuan sekaligus",
-                text: "Dari motorik halus (menggunting, menebalkan garis), kreativitas (mewarnai), sampai fokus dan logika (maze, soal campuran). Semua ada dalam satu media.",
+                img: "/images/funpaper/calistung/funpaper-calistung-4.png",
+                title: "Berhitung konkret",
+                text: "Anak belajar berhitung lewat gambar dan permainan sehingga konsep angka lebih mudah dipahami, bukan sekedar hafalan.",
               },
-              {
-                img: "/images/assets/pensil.avif",
-                heading: "Ilustrasi menarik",
-                text: "Desain visual ramah anak dengan memuat gambar, ikon, atau bentuk yang familiar bagi anak.",
-              },
-              {
-                img: "/images/assets/tas-2.png",
-                heading: "Sesuai tahap perkembangan.",
-                text: "Funpaper bisa dipilih sesuai usia anak (2–6 tahun), jadi bunda tidak perlu bingung lagi mencari lembar aktivitas yang cocok dengan kemampuan si kecil.",
-              },
-              {
-                img: "/images/assets/sekolah-2.avif",
-                heading: "Membuat Anak Lebih Siap Sekolah",
-                text: "Rutin mengerjakan Funpaper Harian melatih konsistensi dan disiplin belajar, sekaligus mengenalkan anak pada keterampilan dasar calistung dengan menyenangkan.",
-              },
-            ].map((solusi, idx) => (
+            ].map((item, idx) => (
               <div
                 key={idx}
-                className="flex flex-col lg:flex-row gap-6 bg-[#fefdf9] rounded-xl py-8 sm:py-14 px-8"
+                className="text-center flex flex-col md:flex-row gap-4 md:gap-14 items-center"
               >
-                <div className="basis-1/4">
+                <div className="flex-1/4">
                   <Image
-                    src={solusi.img}
+                    src={item.img}
+                    alt={item.text}
                     width={200}
-                    height={200}
-                    alt="solusi"
-                    className="max-w-12"
+                    height={300}
+                    className="mx-auto"
                   />
                 </div>
-                <div>
-                  <p className="font-bold mb-2">{solusi.heading}</p>
-                  <p>{solusi.text}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
 
-      {/* Aktivitas List */}
-      <section id="list-funpaper-tema" className="py-24 px-4 bg-[#fefdf9]">
-        <div className="container mx-auto px-4 text-center">
-          <h2 className="text-2xl md:text-3xl font-bold mb-12">
-            Total <span className="text-[#3763A1]">400+</span> Worksheet
-            Pembelajaran Yang Interaktif & Menyenangkan
-          </h2>
-          <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 mb-14">
-            {[
-              {
-                img: "/images/funpaper/harian/funpaper-harian-2.png",
-                text: "Mewarnai",
-              },
-              {
-                img: "/images/funpaper/harian/funpaper-harian-3.png",
-                text: "Menebalkan Garis",
-              },
-              {
-                img: "/images/funpaper/harian/funpaper-harian-4.png",
-                text: "Gunting Tempel",
-              },
-              {
-                img: "/images/funpaper/harian/funpaper-harian-5.png",
-                text: "Soal Campuran",
-              },
-              {
-                img: "/images/funpaper/harian/funpaper-harian-6.png",
-                text: "Maze",
-              },
-              {
-                img: "/images/funpaper/harian/funpaper-harian-7.png",
-                text: "Dan Lain Lain",
-              },
-            ].map((tema, idx) => (
-              <div
-                key={idx}
-                className="px-4 py-10 flex flex-col items-center justify-center bg-[#fbf6f2] rounded-xl"
-              >
-                <Image
-                  src={tema.img}
-                  alt={tema.text}
-                  width={200}
-                  height={120}
-                  className="rounded-lg mb-4"
-                />
-                <p className="mt-3 font-medium text-lg">{tema.text}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <section className="py-24 px-6 bg-[url(/images/bg/bg-harian-2.avif)] bg-cover bg-center bg-no-repeat">
-        <div className="container">
-          <h2 className="text-biru text-xl sm:text-4xl font-bold text-center mb-12">
-            Cocok Untuk Siapa?
-          </h2>
-
-          <div className="mb-44 flex flex-row flex-wrap justify-center gap-8">
-            {[
-              {
-                img: "/images/assets/orang-tua.avif",
-                text: "Orang tua yang cari aktivitas edukatif untuk mengisi waktu di rumah",
-              },
-              {
-                img: "/images/assets/sekolah-3.png",
-                text: "Pelaku homeschooling yang aktif mencari materi ajar tambahan",
-              },
-              {
-                img: "/images/assets/buku-2.png",
-                text: "Guru yang membutuhkan bahan ajar sesuai kurikulum",
-              },
-            ].map((data, idx) => (
-              <div
-                key={idx}
-                className="flex flex-col gap-2 border-2 border-white bg-white/40 rounded-xl py-4 sm:py-8 px-8 items-center shadow-md w-70"
-              >
-                <div className="w-[70px] h-[70px]">
-                  <Image
-                    src={data.img}
-                    width={80}
-                    height={80}
-                    alt="cocok"
-                    className="h-auto"
-                  />
-                </div>
-                <div>
-                  <p className="text-center">{data.text}</p>
+                <div className="bg-[#fefdf9] rounded-2xl py-10 px-8 text-left flex-3/4">
+                  <p className="font-semibold mb-4">{item.title}</p>
+                  <p className="">{item.text}</p>
                 </div>
               </div>
             ))}
@@ -261,19 +175,19 @@ export default function page() {
           <div className="flex flex-col md:flex-row gap-4 md:gap-16 items-center justify-center">
             <div className="">
               <Image
-                src="/images/funpaper/harian/funpaper-harian-1.png"
+                src="/images/funpaper/calistung/funpaper-calistung-5.png"
                 width={700}
                 height={700}
-                alt="Funpaper Tema Campur"
+                alt="Funpaper Calistung"
                 className="mx-auto w-80 md:w-160"
               />
             </div>
             <div className="text-center md:text-left space-y-10">
               <h2 className="font-bold text-xl leading-tight sm:text-3xl lg:text-4xl mb-8 text-center md:text-left">
-                Penuhi Kebutuhan Belajar Ananda Dengan Funpaper Harian
+                Yuk bantu anak siap membaca, menulis, dan berhitung dengan mudah
               </h2>
 
-              <Link href="/funpaper-harian" className="tombol-pink">
+              <Link href="/funpaper-calistung" className="tombol-pink">
                 Download Sekarang
               </Link>
             </div>
@@ -281,10 +195,10 @@ export default function page() {
         </div>
       </section>
 
-      <section className="py-20 px-4 bg-[url(/images/bg/bg-harian-2.avif)] bg-cover bg-no-repeat bg-position-[center_top_-100px] lg:bg-position-[center_top_-300px] min-h-[300px] md:min-h-[400px] flex flex-col justify-center">
+      <section className="py-20 px-4 bg-[url(/images/bg/bg-calistung.jpg)] bg-cover bg-no-repeat bg-position-[center_top_-50px] lg:bg-position-[center_top_-400px] min-h-[300px] md:min-h-[400px] flex flex-col justify-center">
         <div className="container">
-          <h2 className="text-biru text-2xl md:text-4xl font-bold text-center">
-            Temani Ananda Belajar Sampai Bermain
+          <h2 className="text-hijau text-2xl md:text-4xl font-bold text-center">
+            Dengan Funpaper Calistung ananda tak sekedar bisa dan hafal tetapi PAHAM
           </h2>
         </div>
       </section>
